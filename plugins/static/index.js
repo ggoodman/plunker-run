351519add8f715b1bd33cd5871dd91e2e3c1fee6
Geoff Goodman <ggoodman@gmail.com>
2016-09-27 07:51:22 -0400

    Add public cache headers to static assets

--- plugins/static/index.js
+++ plugins/static/index.js
@@ -7,7 +7,7 @@ exports.register = function(server, options, next) {
         config: options.config,
         server: server,
     });
-    
+
     server.dependency([
         'inert',
     ], register);
@@ -31,32 +31,49 @@ function register(server, next) {
         path: '/favicon.ico',
         config: require('./routes/handleStatic')('favicon.ico')
     });
-    
+
     server.route({
         method: 'GET',
         path: '/robots.txt',
         config: require('./routes/handleStatic')('robots.txt')
     });
-    
+
     server.route({
         method: 'GET',
         path: '/sw.js',
         config: require('./routes/handleStatic')('sw.js')
     });
-    
+
     server.route({
         method: 'GET',
         path: '/sw.html',
         config: require('./routes/handleStatic')('sw.html')
     });
-    
+
     server.route({
         method: 'GET',
         path: '/preview.html',
         config: require('./routes/handleStatic')('preview.html')
     });
-    
+
     server.log(['info', 'init'], `Started ${exports.register.attributes.name}@${exports.register.attributes.version}.`);
 
+    server.ext({
+        type: 'onPreResponse',
+        method: addCacheControlHeader,
+        options: {
+            sandbox: 'plugin',
+        },
+    });
+
     next();
+}
+
+
+function addCacheControlHeader(request, reply) {
+    if (request.method === 'get') {
+        request.response.header('Cache-Control', 'max-age=30, must-revalidate, public');
+    }
+
+    reply.continue();
 }
